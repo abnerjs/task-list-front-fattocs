bece41690ec5b01abf5c401d45fce610521b912e
abnerjs <abner.js05@gmail.com>
2024-11-08 20:56:46 -0300

    first commit

---
+++ src/http/server.ts
@@ -0,0 +1,24 @@
+import fastify from 'fastify'
+import {
+  serializerCompiler,
+  validatorCompiler,
+  type ZodTypeProvider,
+} from 'fastify-type-provider-zod'
+import fastifyCors from '@fastify/cors'
+
+const app = fastify().withTypeProvider<ZodTypeProvider>()
+
+app.register(fastifyCors, {
+  origin: '*',
+})
+
+app.setValidatorCompiler(validatorCompiler)
+app.setSerializerCompiler(serializerCompiler)
+
+app
+  .listen({
+    port: 3000,
+  })
+  .then(() => {
+    console.log('HTTP server running!')
+  })
